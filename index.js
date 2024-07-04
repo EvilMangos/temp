de90fa081dc56dadb8cbaa04c4f9d7d273a56ed9
EvilMangos <jchamets@gmail.com>
2024-07-03 20:10:56 -0400

    I am sneaky commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
     sum: (a, b) => a + b,
     difference: (a, b) => a - b,
-    multiple: (a, b) => a * b
+    multiple: (a, b) => a * b,
+    pow: (a, b) => Math.pow(a, b),
+    sqrt: (a) => Math.sqrt(a),
 }
